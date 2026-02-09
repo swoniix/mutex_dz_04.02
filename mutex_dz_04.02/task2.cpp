9b29cd416701d64c337a0697cf9a330d7019a9b1
swoniix <sonyabl909@gmail.com>
2026-02-09 15:44:15 +0200

    commit

--- mutex_dz_04.02/task2.cpp
+++ mutex_dz_04.02/task2.cpp
@@ -3,12 +3,16 @@
 #include <thread>
 #include <windows.h>
 using namespace std;
+
 string textFile[5000];
 int textLinesCount = 0;
+
 string forbiddenWords[500];
 int forbiddenWordsCount = 0;
+
 int totalFound = 0;
 mutex mtx;
+
 bool isSign(char c) {
 	string sign = ".,!?;:'@[]~}{#~$%^&*()-+=/`\\|\"`<>";
 	for (int i = 0; i < sign.length(); i++) {
@@ -25,3 +29,4 @@ string makeLowerCase(string s) {
 	}
 	return result;
 }
+bool loadForbidden(filename = )
